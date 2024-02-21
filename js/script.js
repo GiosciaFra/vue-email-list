fcad3b92cdd5e367deaa3d6834107eb6a7410ba5
giosciafra <francescogioscia8@gmail.com>
2024-02-21 16:03:49 +0100

    aggiunta api , generato 10 emails random e stampati in pagina (esercizio finito senza bonus)

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
+// Descrizione:
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista
+// (non ci interessa che siano tutti diversi).
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
 const { createApp } = Vue
 
 createApp({
     data() {
         return {
-            message: 'hi'
+            emails: [],
+
         }
-    }
-}).mount('#app')
+    },
+
+    mounted() {
+
+        // utilizzo il ciclo for per chiamare 10 volte l'API con axios, ogni richiesta
+        // viene aggiunta all'array [emails]
+        for (let i = 0; i < 10; i++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((result) => {
+                    this.emails.push({ success: result.data.success, 
+                                      response: result.data.response });
+                })
+        }
+    },
+
+    }).mount('#app')
